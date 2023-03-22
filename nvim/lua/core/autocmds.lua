e98df3cac24f6aca8c41ddfe2b0ffa4bc2ace60b
ichigo <dev.honda.ichigo@gmail.com>
2023-03-22 21:43:44 +0900

    feat: make terminal mode easier to use

--- nvim/lua/core/autocmds.lua
+++ nvim/lua/core/autocmds.lua
@@ -5,11 +5,24 @@
 local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
-augroup('setIndent', { clear = true })
-autocmd('Filetype', {
-    group = 'setIndent',
+augroup("setIndent", { clear = true })
+autocmd("Filetype", {
+    group = "setIndent",
     pattern = {
-        'xml', 'html', 'xhtml'
+        "xml", "html", "xhtml"
     },
-    command = 'setlocal shiftwidth=1 tabstop=1'
+    command = "setlocal shiftwidth=1 tabstop=1",
 })
+
+-- terminal
+
+autocmd("TermOpen", { command = "startinsert" })
+autocmd("TermOpen", { command = "setlocal norelativenumber" })
+autocmd("TermOpen", { command = "setlocal nonumber" })
+
+autocmd("BufEnter", {
+    pattern = "term://*",
+    command = "startinsert",
+})
+
+autocmd("TermClose", { command = "q" })
